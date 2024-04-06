7d22978338a3a7ca86ee6085ce0ef836a0ede691
Rowan Kelleher <rowan.kelleher@duke.edu>
2024-04-06 18:04:07 -0400

    added error bars to pi_5GeV_energy_run1.C and mu_1GeV_energy.C, seem maybe okay but may be some bugs

--- macros/mu_1GeV_energy.C
+++ macros/mu_1GeV_energy.C
@@ -8,10 +8,16 @@ void mu_1GeV_energy::Loop()
 
 {
     if (fChain == 0) return;
-   
+
+    /*DEBUG*/
+    Float_t max = -999999;
+    Float_t min = 9999999;
+    /*ENDDEBUG*/
+
+    
     int num_layers = 28;
     int cont = 0;
-    Float_t layer_map[28] = {1830.8000, 1841.4000, 1907.5, 1918.0999,1984.1999, 1994.8000, 2060.8999,2071.5,2137.6001,2148.1999,2214.3000,2224.8999,2291,2301.6001,2367.6999,2378.3000,2444.3999,2455,2521.1001,2531.6999,2597.8000,2608.3999,2674.5,2685.1001,2751.1999,2761.8000,2827.8999,2838.5}; //x value of each scintillator layer - use to assign hits to layer
+    Float_t layer_map[28] = {1830.8000, 1841.4000, 1907.5, 1918.1,1984.1999, 1994.8000, 2060.8999,2071.5,2137.6001,2148.1999,2214.3000,2224.8999,2291,2301.6001,2367.6999,2378.3000,2444.3999,2455,2521.1001,2531.6999,2597.8000,2608.3999,2674.5,2685.1001,2751.1999,2761.8000,2827.8999,2838.5}; //x value of each scintillator layer - use to assign hits to layer
     Float_t energy_arr[28] = {}; //Set equal to {} to ensure initialize to 0 in each entry
 
     long curr_layer; //use for each hit to find which layer the hit hit
@@ -37,7 +43,15 @@ void mu_1GeV_energy::Loop()
 	if(curr_layer == -1){continue;}
 	//if(curr_layer == -1){cont++; cout << "continuing #" << cont << "\n"; continue;} //skip any hits that don't correspond to a layer (assumes that hits are always at the same position)
 	energy_arr[curr_layer] += HcalBarrelHits_energy[i]; //sum up all hits that correspond to 1 layer
+
 	
+	/*DEBUG*/
+	if(HcalBarrelHits_energy[i] > max) {max = HcalBarrelHits_energy[i];}
+	if(HcalBarrelHits_energy[i] < min) {min = HcalBarrelHits_energy[i];}
+
+	/*ENDDEBUG*/
+
+	   
 	curr_layer = -1; //used to check if the hit corresponds to a layer or not. May want to bin instead
       }
       curr_event++;
@@ -49,9 +63,44 @@ void mu_1GeV_energy::Loop()
      avg_energy[i] = energy_arr[i] / nentries; //HcalBarrelHits is the number of hits given by the root tree
      //cout << "sum energy in bin #" << i << ": " << energy_arr[i] << "\n";
    }
+
+   // Calculate Standard deviation
    
+   Float_t y_err[28] = {};
+   Float_t x_err[28] = {};
+
+   Float_t sq_residuals[28] = {};
+
+   //Loop over events
+   for(int entry = 0; entry < nentries; entry++) {
+     Float_t error_energy_arr[28] = {};
+     b_HcalBarrelHits_->GetEntry(entry);
+     b_HcalBarrelHits_energy->GetEntry(entry);
+     //Loop over hits
+     for(int i = 0; i < HcalBarrelHits_; i++) {
+       Float_t curr_x = HcalBarrelHits_position_x[i];
+       for(int j = 0; j < num_layers; j++) {
+	 //need to find the layer of this particular hit
+	 if(layer_map[j] == curr_x) {
+	   curr_layer = j;
+	   break; //can break after we found the layer
+	 }
+       }
+       if(curr_layer == -1){continue;}
+       error_energy_arr[curr_layer] += HcalBarrelHits_energy[i];
+       //Calculate residual
+       
+       curr_layer = -1; //used to check if the hit corresponds to a layer or not. May want to bin instead
+      }
+     for(int layer = 0; layer < num_layers; layer++) {
+       sq_residuals[layer] += pow(error_energy_arr[layer] - avg_energy[layer],2);
+     }
+   }
+   for(int i = 0; i < num_layers; i++) {
+     y_err[i] = pow(sq_residuals[i] / nentries,0.5);
+   }
    TCanvas *c1 = new TCanvas();
-   TGraph *gr = new TGraph(num_layers, layer_map, avg_energy);
+   TGraph *gr = new TGraphErrors(num_layers,layer_map, avg_energy,x_err, y_err);
    
    gr->SetTitle("avg energy deposition in each layer 10000 events (1GeV mu- gun)");
    auto xaxis = gr->GetXaxis();
@@ -65,7 +114,7 @@ void mu_1GeV_energy::Loop()
    gr->SetMarkerColor(2);
    gr->Draw("AP");
    
-   TFile f("root_files/graphs/april_5/mu_1GeV_energy_run1.root","recreate");
+   TFile f("root_files/graphs/april_6/mu_1GeV_energy_run1.root","recreate");
    gr->Write();
-   c1->Print("plots/april_5/mu_1GeV_energy_run1.pdf");
+   c1->Print("plots/april_6/mu_1GeV_energy_run1.pdf");
 }
--- macros/mu_5GeV_energy_run1.C
+++ macros/mu_5GeV_energy_run1.C
@@ -5,15 +5,19 @@
 #include <TCanvas.h>
 
 void mu_5GeV_energy_run1::Loop()
+
 {
     if (fChain == 0) return;
-   
+
+    /*DEBUG*/
+    Float_t max = -999999;
+    Float_t min = 9999999;
+    /*ENDDEBUG*/
+
+    
     int num_layers = 28;
     int cont = 0;
     Float_t layer_map[28] = {1830.8000, 1841.4000, 1907.5, 1918.1,1984.1999, 1994.8000, 2060.8999,2071.5,2137.6001,2148.1999,2214.3000,2224.8999,2291,2301.6001,2367.6999,2378.3000,2444.3999,2455,2521.1001,2531.6999,2597.8000,2608.3999,2674.5,2685.1001,2751.1999,2761.8000,2827.8999,2838.5}; //x value of each scintillator layer - use to assign hits to layer
-
-    //weird - in root file the index 3 layer has value 1918.0999 but these get rounded to 1918.1 for some reason, while the others are not rounded
-    
     Float_t energy_arr[28] = {}; //Set equal to {} to ensure initialize to 0 in each entry
 
     long curr_layer; //use for each hit to find which layer the hit hit
@@ -29,7 +33,6 @@ void mu_5GeV_energy_run1::Loop()
       nb = fChain->GetEntry(jentry);   nbytes += nb;
       for(int i = 0; i < HcalBarrelHits_; i++) {
 	Float_t curr_x = HcalBarrelHits_position_x[i];
-	//cout << "curr_x: " << curr_x << "\n";
 	for(int j = 0; j < num_layers; j++) {
 	  //need to find the layer of this particular hit
 	  if(layer_map[j] == curr_x) {
@@ -37,23 +40,67 @@ void mu_5GeV_energy_run1::Loop()
 	    break; //can break after we found the layer
 	  }
 	}
-	if(curr_layer == -1){cout << "no layer found: " << curr_x<< "\n";continue;}
+	if(curr_layer == -1){continue;}
+	//if(curr_layer == -1){cont++; cout << "continuing #" << cont << "\n"; continue;} //skip any hits that don't correspond to a layer (assumes that hits are always at the same position)
 	energy_arr[curr_layer] += HcalBarrelHits_energy[i]; //sum up all hits that correspond to 1 layer
+
 	
+	/*DEBUG*/
+	if(HcalBarrelHits_energy[i] > max) {max = HcalBarrelHits_energy[i];}
+	if(HcalBarrelHits_energy[i] < min) {min = HcalBarrelHits_energy[i];}
+
+	/*ENDDEBUG*/
+
+	   
 	curr_layer = -1; //used to check if the hit corresponds to a layer or not. May want to bin instead
       }
       curr_event++;
    }
-   cout << "processed " << curr_event << "events\n";
+   
    //average out the sums
    Float_t avg_energy[28];
    for(int i = 0; i < num_layers; i++) {
      avg_energy[i] = energy_arr[i] / nentries; //HcalBarrelHits is the number of hits given by the root tree
+     //cout << "sum energy in bin #" << i << ": " << energy_arr[i] << "\n";
    }
-   TCanvas *c1 = new TCanvas("c1", "c1",955,600);
-   c1->SetLeftMargin(0.13);
-   c1->SetRightMargin(0.04);
-   TGraph *gr = new TGraph(num_layers, layer_map, avg_energy);
+
+   // Calculate Standard deviation
+   
+   Float_t y_err[28] = {};
+   Float_t x_err[28] = {};
+
+   Float_t sq_residuals[28] = {};
+
+   //Loop over events
+   for(int entry = 0; entry < nentries; entry++) {
+     Float_t error_energy_arr[28] = {};
+     b_HcalBarrelHits_->GetEntry(entry);
+     b_HcalBarrelHits_energy->GetEntry(entry);
+     //Loop over hits
+     for(int i = 0; i < HcalBarrelHits_; i++) {
+       Float_t curr_x = HcalBarrelHits_position_x[i];
+       for(int j = 0; j < num_layers; j++) {
+	 //need to find the layer of this particular hit
+	 if(layer_map[j] == curr_x) {
+	   curr_layer = j;
+	   break; //can break after we found the layer
+	 }
+       }
+       if(curr_layer == -1){continue;}
+       error_energy_arr[curr_layer] += HcalBarrelHits_energy[i];
+       //Calculate residual
+       
+       curr_layer = -1; //used to check if the hit corresponds to a layer or not. May want to bin instead
+      }
+     for(int layer = 0; layer < num_layers; layer++) {
+       sq_residuals[layer] += pow(error_energy_arr[layer] - avg_energy[layer],2);
+     }
+   }
+   for(int i = 0; i < num_layers; i++) {
+     y_err[i] = pow(sq_residuals[i] / nentries,0.5);
+   }
+   TCanvas *c1 = new TCanvas();
+   TGraph *gr = new TGraphErrors(num_layers,layer_map, avg_energy,x_err, y_err);
    
    gr->SetTitle("avg energy deposition in each layer 10000 events (5GeV mu- gun)");
    auto xaxis = gr->GetXaxis();
@@ -68,8 +115,10 @@ void mu_5GeV_energy_run1::Loop()
    gr->SetMarkerSize(0.8);
    gr->SetMarkerColor(4);
    gr->Draw("AP");
+
+   cout << "nentries: " << nentries << "Max: " << max << "| Min: " << min << "\n";
    
-   //TFile f("root_files/graphs/april_5/mu_5GeV_energy_run1.root","recreate");
+   //TFile f("root_files/graphs/april_6/mu_5GeV_energy_run1.root","recreate");
    //gr->Write();
-   //c1->Print("plots/april_5/mu_5GeV_energy_run1.svg");
+   //c1->Print("plots/april_6/mu_5GeV_energy_run1.svg");
 }
