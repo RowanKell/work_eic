b114aca23134aab65389b511f1dc4856b1c156f7
Rowan Kelleher <rck32@dcc-login-04.oit.duke.edu>
2024-05-24 14:57:19 -0400

    looking into basic ML model and slurm scripts

--- slurm/job.sh
+++ slurm/job.sh
@@ -1,20 +1,17 @@
-#!/bin/tcsh                                                                                                                                                                
-#SBATCH --account=clas12
-#SBATCH --partition=production
-#SBATCH --mem-per-cpu=4000
+#!/bin/bash  
 #SBATCH --job-name=KLM_sim
+#SBATCH --output=/hpc/group/vossenlab/rck32/eic/work_eic/slurm/output/outputMay20/%x.out
+#SBATCH --error=/hpc/group/vossenlab/rck32/eic/work_eic/slurm/error/errorMay20/%x.err
+#SBATCH -p vossenlab-gpu
+#SBATCH --account=vossenlab
 #SBATCH --cpus-per-task=1
-#SBATCH --time=24:00:00
-#SBATCH --output=/u/home/rojokell/work_dir/slurm/output/outputMay_20/%x.out                                                                               
-#SBATCH --error=/u/home/rojokell/work_dir/slurm/error/errorMay_20/%x.err
-module purge -f
-source /u/home/rojokell/.cshrc
-
-cd /u/home/rojokell/eic
-./eic-shell
-cd epic_klm
+#SBATCH --mem=8G
+#SBATCH --mail-user=rck32@duke.edu
+echo began job
+cd /hpc/group/vossenlab/rck32/eic/epic_klm
+cat << EOF | /hpc/group/vossenlab/rck32/eic/eic-shell
 source install/setup.sh
-
-ddsim --steeringFile simulations/steering/npsim_local3.py --compactFile $DETECTOR_PATH/epic_klmws_only.xml -G -N 10 --gun.particle "mu-" --outputFile ../work_eic/root_files/May9/mu_5GeV_10events_run_1.edm4hep.root --part.userParticleHandler=""
+/usr/local/bin/ddsim --steeringFile simulations/steering/npsim_local3.py --compactFile /hpc/group/vossenlab/rck32/eic/epic_klm/epic_klmws_only.xml -G -N 30 --gun.particle "mu-" --outputFile ../work_eic/root_files/May21/mu_5GeV_30events_run_1.edm4hep.root --part.userParticleHandler=""
+EOF
 
 
