3fe64dea7083ce53ec0d663b2d5f5bfcd7341529
Rowan Kelleher <rck32@dcc-login-05.oit.duke.edu>
2024-06-04 16:49:02 -0400

    updates

--- slurm/time_resolution.sh
+++ slurm/time_resolution.sh
@@ -2,13 +2,13 @@
 #SBATCH --account=vossenlab
 #SBATCH -p common
 #SBATCH --mem=30G
-#SBATCH --job-name=mu_one_segment_5GeV
+#SBATCH --job-name=pi_one_segment_5GeV_1_5m
 #SBATCH --cpus-per-task=1
 #SBATCH --time=24:00:00
 #SBATCH --chdir=/cwork/rck32/eic/epic_klm
-#SBATCH --output=/cwork/rck32/eic/work_eic/slurm/output/outputMay_31/%x.out
-#SBATCH --error=/cwork/rck32/eic/work_eic/slurm/error/errorMay_31/%x.err
+#SBATCH --output=/cwork/rck32/eic/work_eic/slurm/output/outputJun_3/%x.out
+#SBATCH --error=/cwork/rck32/eic/work_eic/slurm/error/errorJun_3/%x.err
 cat << EOF | /cwork/rck32/eic/eic-shell
 source install/setup.sh
-/usr/local/bin/ddsim --steeringFile /cwork/rck32/eic/work_eic/steering/steer.py --compactFile /cwork/rck32/eic/epic_klm/epic_klmws_only.xml -G -N 10000 --gun.particle "mu-" --outputFile ../work_eic/root_files/time_res/one_segment/mu_5GeV_10000events_2_5m_1cm_3cm.edm4hep.root --part.userParticleHandler=""
+/usr/local/bin/ddsim --steeringFile /cwork/rck32/eic/work_eic/steering/steer.py --compactFile /cwork/rck32/eic/epic_klm/epic_klmws_only.xml -G -N 1000 --gun.particle "pi-" --outputFile ../work_eic/root_files/time_res/one_segment_sensor_in_volume/pi_5GeV_1kevents_1_5m_1cm_3cm_new_q_eff.edm4hep.root --part.userParticleHandler=""
 EOF
