e2c8877850cab6b134367b829d7078d7b22252d2
Rowan Kelleher <rck32@dcc-login-04.oit.duke.edu>
2024-06-14 14:24:00 -0400

    updates to variation

--- ddsim_shells/variation.sh
+++ ddsim_shells/variation.sh
@@ -1,1 +1,1 @@
-ddsim --steeringFile steering/variation.py --compactFile $DETECTOR_PATH/epic_klmws_only.xml --runType "batch" -G -N 100 --gun.particle "mu-" --outputFile root_files/June_13/variation/full_mu_5GeV_100.edm4hep.root --macroFile ddsim_shells/myvis.mac --part.userParticleHandler="" 
+ddsim --steeringFile steering/variation.py --compactFile $DETECTOR_PATH/epic_klmws_only.xml --runType "batch" -G -N 100 --gun.particle "mu-" --outputFile root_files/June_14/variation/test_mu_5GeV_100.edm4hep.root --macroFile ddsim_shells/myvis.mac --part.userParticleHandler="" 
